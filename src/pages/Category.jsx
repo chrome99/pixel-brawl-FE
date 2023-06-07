04dfb9d4cddcc199a9d4cceaf7e61ee986bdbd90
Noa Sela <noalina.sela@gmail.com>
2023-06-07 23:04:13 +0300

    user scores

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,8 +12,9 @@ const Category = () => {
   const params = useParams();
 
   const [scores , setScores] = useState([])
+  const [scoreSum , setScoreSum] = useState([])
   const {user} = useContext(AuthContext)
-  const username = user.username
+  const username = user?.username
 
   useEffect(()=>{
     if (params.categoryName === "scores") {
@@ -26,11 +27,30 @@ const Category = () => {
     if (params.categoryName === "user-scores"){
       (async()=>{
         const scores = await getScoresByUser(username)
+        const scoreSum = getPlayerScoreSum(username, scores)
         setScores(scores)
+        setScoreSum(scoreSum)
       })()
     }
 
-  }, [params])
+  }, [params, user])
+
+
+  function getPlayerScoreSum(playerName, data) {
+    let sum = 0;
+    
+    for (let i = 0; i < data.length; i++) {
+      const game = data[i];
+      
+      if (game.player1 === playerName) {
+        sum += game.player1Score;
+      } else if (game.player2 === playerName) {
+        sum += game.player2Score;
+      }
+    }
+    
+    return sum;
+  }
   
   return (
     <div>
@@ -45,9 +65,10 @@ const Category = () => {
             </h3>
           </header>
           <TableRow index={""} player1={"player1"} player2={"player2"} score={"score"}/>
-          <div>
+          <div className='tableBody'>
           {scores. map((s, index) => <TableRow index={index+1} player1={s.player1} player2={s.player2} score={`${s.player1Score}/${s.player2Score}`}/>)}
           </div>
+          {params.categoryName === "user-scores" && <p className='userScore'>{`Total Score: ${scoreSum}`}</p>}
         </div>
       </div>
     </div>
