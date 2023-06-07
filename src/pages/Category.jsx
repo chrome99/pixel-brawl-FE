52feca54b7bf828b432b9fa966ad9902d4160cb8
Noa Sela <noalina.sela@gmail.com>
2023-06-07 22:37:38 +0300

    table-done

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Navbar from '../components/Navbar';
 import { useParams } from "react-router-dom";
 import "../App.css"
 import "./category.css"
 import TableRow from '../components/ScoreTable/TableRow';
+import { getAllScores, getScoresByUser } from '../services/scoreServices';
+import { AuthContext } from '../contexts/AuthContext';
 
 
 const Category = () => {
   const params = useParams();
+
+  const [scores , setScores] = useState([])
+  const {user} = useContext(AuthContext)
+  const username = user.username
+
+  useEffect(()=>{
+    if (params.categoryName === "scores") {
+      (async()=>{
+        const scores = await getAllScores()
+        setScores(scores)
+      })()
+    }
+
+    if (params.categoryName === "user-scores"){
+      (async()=>{
+        const scores = await getScoresByUser(username)
+        setScores(scores)
+      })()
+    }
+
+  }, [params])
   
   return (
     <div>
@@ -21,8 +44,11 @@ const Category = () => {
                 : "My Scores"}
             </h3>
           </header>
-          {/* create a condition for each param */}
-          <TableRow />
+          
+          <TableRow index={""} player1={"player1"} player2={"player2"} score={"score"}/>
+          <div>
+          {scores. map((s, index) => <TableRow index={index+1} player1={s.player1} player2={s.player2} score={`${s.player1Score}/${s.player2Score}`}/>)}
+          </div>
         </div>
       </div>
     </div>
